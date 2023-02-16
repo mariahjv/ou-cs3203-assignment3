6df0865d8e323c10bb8f60755cc6923499268002
mariahjv <mariah.vadak@ou.edu>
2023-02-16 01:06:24 -0600

    Add main function

--- Assignment_3.py
+++ Assignment_3.py
@@ -13,3 +13,23 @@ def multiply(nums):
         product = product * i
 
     return (product)
+
+def main():
+    nums = []
+    user = input("Enter a number or 'done' to finish): ")
+    while user != "done":
+        nums.append(int(user))
+        user = input("Enter a number or 'done' to finish: ")
+
+    if len(nums) == None:
+        print("No numbers were entered.")
+    else:
+        operation = input("Enter 'sum', or 'multiply': ")
+        if operation == "sum":
+            result = sums(nums)
+            print(f"The sum of {nums} is {result}")
+        elif operation == "multiply":
+            result = multiply(nums)
+            print(f"The product of {nums} is {result}")
+        else:
+            print("Invalid operation. Please enter 'sum' or 'multiply'.")
